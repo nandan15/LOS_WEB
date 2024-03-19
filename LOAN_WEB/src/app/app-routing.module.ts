5fae10dbf669d00fdaf474341431bed29e16321e
srinivas <srinivasbk850@gmail.com>
2024-03-19 18:12:58 +0530

    sidebar updated

--- LOAN_WEB/src/app/app-routing.module.ts
+++ LOAN_WEB/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sidebar',loadChildren:()=>import('../app/modules/shared/shared.module').then(m=>m.SharedModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- LOAN_WEB/src/app/modules/shared/shared.module.ts
+++ LOAN_WEB/src/app/modules/shared/shared.module.ts
@@ -14,6 +14,9 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
