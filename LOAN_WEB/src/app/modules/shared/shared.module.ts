29781a47f6db03c890ee20f4dcfc6e7c3688110b
Nandan Kumar <nandan@likhitech.com>
2024-03-19 17:27:15 +0530

    shared Module Added

---
+++ LOAN_WEB/src/app/modules/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedComponent } from './shared/shared.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    SharedComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
